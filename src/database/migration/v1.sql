671f5b939797ed2bbf57d3d40750cf516453d454
UM <romain.meuter@lytn.io>
2024-09-10 12:22:47 +0200

    modify(change col to possible null col)

--- src/database/migration/v1.sql
+++ src/database/migration/v1.sql
@@ -1,46 +1,43 @@
--- Création des tables avec clés primaires et étrangères
-
--- Table movie
 CREATE TABLE "movie" (
-  "movieId" INTEGER PRIMARY KEY,  -- Clé primaire
-  "imdbId" INTEGER,
-  "tmdbId" REAL,
-  "title" TEXT,
-  "genres" TEXT
+  "movieId" INTEGER PRIMARY KEY,  
+  "imdbId" INTEGER NULL,
+  "tmdbId" REAL NULL,
+  "title" TEXT NULL,
+  "genres" TEXT NULL
 );
 
--- Table tag
+
 CREATE TABLE "tag" (
-  "tagId" INTEGER PRIMARY KEY,  -- Clé primaire
-  "tag" TEXT
+  "tagId" INTEGER PRIMARY KEY,  
+  "tag" TEXT NULL
 );
 
--- Table movie_tag
+
 CREATE TABLE "movie_tags" (
-  "movieId" INTEGER,  -- Clé étrangère vers movie
-  "tagId" INTEGER,    -- Clé étrangère vers tag
-  "relevance" REAL,
-  PRIMARY KEY ("movieId", "tagId"),  -- Clé primaire composite
+  "movieId" INTEGER,  
+  "tagId" INTEGER,    
+  "relevance" REAL NULL,
+  PRIMARY KEY ("movieId", "tagId"),  
   FOREIGN KEY ("movieId") REFERENCES "movie" ("movieId") ON DELETE CASCADE,
   FOREIGN KEY ("tagId") REFERENCES "tag" ("tagId") ON DELETE CASCADE
 );
 
--- Table movie_user_tag
+
 CREATE TABLE "movie_user_tag" (
   "userId" INTEGER,
-  "movieId" INTEGER,  -- Clé étrangère vers movie
-  "tag" TEXT,
-  "timestamp" INTEGER,
-  PRIMARY KEY ("userId", "movieId", "tag"),  -- Clé primaire composite
+  "movieId" INTEGER,  
+  "tag" TEXT NULL,
+  "timestamp" INTEGER NULL,
+  PRIMARY KEY ("userId", "movieId"),  
   FOREIGN KEY ("movieId") REFERENCES "movie" ("movieId") ON DELETE CASCADE
 );
 
--- Table movie_user_rating
+
 CREATE TABLE "movie_user_rating" (
   "userId" INTEGER,
-  "movieId" INTEGER,  -- Clé étrangère vers movie
-  "rating" REAL,
-  "timestamp" INTEGER,
-  PRIMARY KEY ("userId", "movieId"),  -- Clé primaire composite
+  "movieId" INTEGER,  
+  "rating" REAL  NULL,
+  "timestamp" INTEGER NULL,
+  PRIMARY KEY ("userId", "movieId"), 
   FOREIGN KEY ("movieId") REFERENCES "movie" ("movieId") ON DELETE CASCADE
 );
