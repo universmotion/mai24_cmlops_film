7836d52911107a0c7be3748b7063e1cfc7aaa2c2
UM <romain.meuter@lytn.io>
2024-09-16 17:32:00 +0200

    modify(adapte package and parameters for kubernetes)

--- src/API/app/db_manager.py
+++ src/API/app/db_manager.py
@@ -5,9 +5,9 @@ from datamodel import Client
 
 db_user = os.environ["DB_USER"]
 db_password = os.environ["DB_PASSWORD"]
-db_host = os.environ["DATABASE_HOST"]
-db_port = os.environ["DATABASE_PORT"]
-db_database = os.environ["DB_DATABASE"]
+db_host = os.environ["DB_HOST"]
+db_port = os.environ["DB_PORT"]
+db_database = os.environ["DB_NAME"]
 SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_database}"
 
 engine = create_engine(
