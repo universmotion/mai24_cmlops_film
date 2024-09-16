8f7caa6d9bb53726592ba7b77bb6b93b6c3439ed
UM <romain.meuter@lytn.io>
2024-09-16 15:16:29 +0200

    add(new migration v2)

---
+++ src/database/migration/v2.sql
@@ -0,0 +1,51 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "movies" (
+  "movieId" INTEGER PRIMARY KEY,  
+  "title" VARCHAR NULL,
+  "genres" VARCHAR NULL
+);
+
+
+CREATE TABLE "users" (
+  "userId" SERIAL PRIMARY KEY,  
+  "count_movies" INTEGER DEFAULT 0,
+  "(no genres listed)" REAL DEFAULT 0,
+  "Action" REAL DEFAULT 0,
+  "Adventure" REAL DEFAULT 0,
+  "Animation" REAL DEFAULT 0,
+  "Children" REAL DEFAULT 0,
+  "Comedy" REAL DEFAULT 0,
+  "Crime" REAL DEFAULT 0,
+  "Documentary" REAL DEFAULT 0,
+  "Drama" REAL DEFAULT 0,
+  "Fantasy" REAL DEFAULT 0,
+  "Film-Noir" REAL DEFAULT 0,
+  "Horror" REAL DEFAULT 0,
+  "IMAX" REAL DEFAULT 0,
+  "Musical" REAL DEFAULT 0,
+  "Mystery" REAL DEFAULT 0,
+  "Romance" REAL DEFAULT 0,
+  "Sci-Fi" REAL DEFAULT 0,
+  "Thriller" REAL DEFAULT 0,
+  "War" REAL DEFAULT 0,
+  "Western" REAL DEFAULT 0
+);
+
+CREATE TABLE "movies_users_rating" (
+  "userId" INTEGER,
+  "movieId" INTEGER,  
+  "rating" REAL  NULL,
+  "timestamp" INTEGER NULL,
+  PRIMARY KEY ("userId", "movieId"), 
+  FOREIGN KEY ("movieId") REFERENCES "movies" ("movieId") ON DELETE cascade,
+  FOREIGN KEY ("userId") REFERENCES "users" ("userId") ON DELETE CASCADE
+);
+
+CREATE TABLE clients (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), 
+    username VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL
+);
--- src/database/setup-db.sh
+++ src/database/setup-db.sh
@@ -1,6 +1,3 @@
-## Install kube
-## curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.29.5+k3s1 sh -s - --write-kubeconfig-mode 644
-
 kubectl create namespace sys-reco-dev
 sudo kubectl config set-context --current --namespace=sys-reco-dev
 
