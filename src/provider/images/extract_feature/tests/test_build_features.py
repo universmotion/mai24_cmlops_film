1fd2c10d81a16b350b92545fbdf2ce263149a819
UM <romain.meuter@lytn.io>
2024-09-18 13:12:45 +0200

    add(user_matrix df to compare); modify(add assert equal to df user_matrix)

--- src/provider/images/extract_feature/tests/test_build_features.py
+++ src/provider/images/extract_feature/tests/test_build_features.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 from build_features import read_ratings, read_movies, create_user_matrix
 
-TEST_DATA_DIR = "tests/data"
+TEST_DATA_DIR = "./src/provider/images/extract_feature/tests/data"
 
 @pytest.fixture
 def movies_csv_path():
@@ -22,29 +22,34 @@ def ratings_df():
 def movies_df():
     return pd.read_csv(os.path.join(TEST_DATA_DIR, "movies_readed.csv"))
 
+@pytest.fixture
+def user_matrix_df():
+    return pd.read_csv(os.path.join(TEST_DATA_DIR, "user_matrix.csv"))
+
 def test_read_ratings(ratings_csv_path, ratings_df):
     result = read_ratings(ratings_csv_path, data_dir=TEST_DATA_DIR)
     
-    assert list(result.columns) == ["userId", "movieId", "rating", "timestamp"]
+    assert list(result.columns).sort() == list(ratings_df.columns).sort()
 
     assert result.isnull().sum().sum() == 0
 
-    assert (result == ratings_df).all().all()  
+    pd.testing.assert_frame_equal(result, ratings_df)  
 
 def test_read_movies(movies_csv_path, movies_df):
     result = read_movies(movies_csv_path, data_dir=TEST_DATA_DIR)
 
-    assert list(result.columns) == list(movies_df.columns)
+    assert list(result.columns).sort() == list(movies_df.columns).sort()
 
     assert result.isnull().sum().sum() == 0
-    assert (result == movies_df).all().all()   
 
-def test_create_user_matrix(ratings_df, movies_df):
+    pd.testing.assert_frame_equal(result,  movies_df)   
+
+def test_create_user_matrix(ratings_df, movies_df, user_matrix_df):
     result = create_user_matrix(ratings_df, movies_df)
+    
+    assert list(result.columns).sort() == list(user_matrix_df.columns).sort()
 
-    assert not result.empty
+    assert result.isnull().sum().sum() == 0
 
-    assert 1 in result.index
+    pd.testing.assert_frame_equal(result.reset_index(drop=True), user_matrix_df.reset_index(drop=True))
 
-    assert "Adventure" in result.columns
-    assert "Comedy" in result.columns
