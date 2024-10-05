072cfa2a4727e250fe2b8b8323d0d51e76f9167a
UM <romain.meuter@lytn.io>
2024-10-05 18:01:54 +0200

    add(dvc tracker)

--- src/database/setup-db.sh
+++ src/database/setup-db.sh
@@ -1,10 +1,10 @@
 kubectl create namespace database
 sudo kubectl config set-context --current --namespace=database
 
-kubectl apply -f ./src/database/DB/pv-pvc-postgres.yaml
-kubectl apply -f ./src/database/DB/service.yaml
-kubectl apply -f ./src/database/DB/secret.yaml 
-kubectl apply -f ./src/database/DB/configmap.yaml 
-kubectl apply -f ./src/database/DB/stateful.yaml 
+kubectl apply -f ./src/database/kubernetes/pv-pvc-postgres.yaml
+kubectl apply -f ./src/database/kubernetes/service.yaml
+kubectl apply -f ./src/database/kubernetes/secret.yaml 
+kubectl apply -f ./src/database/kubernetes/configmap.yaml 
+kubectl apply -f ./src/database/kubernetes/stateful.yaml 
 
 ## psql -h 10.43.150.34 -p 5432 -U admin -d postgresdb
--- src/monitoring/setup-monitoring.sh
+++ src/monitoring/setup-monitoring.sh
@@ -3,9 +3,11 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 # Dashboard
-kubectl apply -f ./src/monitoring/grafana/dashboard/cm-dashboard-model_user.yaml
+kubectl apply -f ./src/monitoring/grafana/dashboard/cm-model.yaml
+kubectl apply -f ./src/monitoring/grafana/dashboard/cm-api.yaml
+kubectl apply -f ./src/monitoring/grafana/dashboard/cm-database.yaml
 
 helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring  --create-namespace --set grafana.service.type=NodePort --set promotheus.service.type=NodePort
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f ./src/monitoring/custom_values.yaml -n monitoring
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f ./src/monitoring/custom_values.yaml -n monitoring --debug
 
 # helm uninstall prometheus -n monitoring
