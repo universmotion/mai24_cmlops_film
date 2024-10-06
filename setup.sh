6811c52728887104bcc5d8f45b150cf6a7c7aef2
UM <romain.meuter@lytn.io>
2024-10-06 12:45:08 +0200

    modify(dashboard grafana custom and add panel, tag, etc...)

--- setup.sh
+++ setup.sh
@@ -10,18 +10,27 @@
 # sudo apt-get update
 # sudo apt-get install helm=3.15.1-1
 
+## Stop
+kubectl -n database delete all --all
+kubectl -n api delete all --all
+helm uninstall airflow -n airflow 
+helm uninstall prometheus -n monitoring 
+
+
 if [ ! -d "./data" ]; then
     mkdir data;
 fi
 
+echo "\n\n\n ### Installation de la base de données"
 bash ./src/database/setup-db.sh
+
+echo "\n\n\n ### Installation de l'api"
 bash ./src/API/setup-api.sh
+
+echo "\n\n\n ### Installation d'airflow"
 bash ./src/provider/setup-airflow.sh
+
+echo "\n\n\n ### Installation du monitoring"
 bash ./src/monitoring/setup-monitoring.sh
 
-## Stop
-# kubectl -n database delete all --all
-# kubectl -n api delete all --all
-# helm uninstall airflow -n airflow 
-# helm uninstall prometheus -n monitoring 
 
--- src/monitoring/setup-monitoring.sh
+++ src/monitoring/setup-monitoring.sh
@@ -3,11 +3,10 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 # Dashboard
-kubectl apply -f ./src/monitoring/grafana/dashboard/cm-model.yaml
-kubectl apply -f ./src/monitoring/grafana/dashboard/cm-api.yaml
-kubectl apply -f ./src/monitoring/grafana/dashboard/cm-database.yaml
+for file in ./src/monitoring/grafana/dashboard/*.yaml
+do
+  kubectl apply -f $file
+done
 
-helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring  --create-namespace --set grafana.service.type=NodePort --set promotheus.service.type=NodePort
+# helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring  --create-namespace --set grafana.service.type=NodePort --set promotheus.service.type=NodePort
 helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -f ./src/monitoring/custom_values.yaml -n monitoring --debug
-
-# helm uninstall prometheus -n monitoring
