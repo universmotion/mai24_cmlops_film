d9239d675d05cb431098a442faabe189203920c9
UM <romain.meuter@lytn.io>
2024-10-08 21:57:52 +0200

    modify(description and remove mlflow package to api)

--- src/API/app/description.py
+++ src/API/app/description.py
@@ -1,10 +1,10 @@
 description = """ # Documentation de l'API de Recommandation de Films
 ## Description
 Cette API permet de gérer un système de recommandations de films pour les utilisateurs. Elle gère les utilisateurs, leurs historiques de films, et propose des recommandations basées sur les films déjà vus.
-Les clients sont les divers plateformes de streaming qui souhaites que leur utiliseurs reçoit des recommandations. 
+Les clients sont les divers plateformes de streaming qui souhaites que leur utiliseurs reçoit des recommandations.
 
 ### Remarque 
-Attentions, les clients ne sont pas utilisateurs. Au sein de l'api, les 
+Attentions, les clients ne sont pas utilisateurs. Au sein de l'api, les clients sont des plateformes comme netflix, prime, etc...
 
 ## Authentification
 L'API utilise un système d'authentification OAuth2 avec des tokens JWT. Les utilisateurs doivent être authentifiés pour accéder aux routes protégées.
