1914397d4d1e0b90169ec4d6d2f3f06abab63034
UM <romain.meuter@lytn.io>
2024-10-09 16:48:12 +0200

    modify/fix(affluence simulation; fix request error)

--- src/simulator/main.py
+++ src/simulator/main.py
@@ -3,12 +3,14 @@ import random
 from datetime import datetime
 import math
 import time
+from pathlib import Path
+import pandas as pd
 
 print("### "*20)
 print(datetime.now().strftime('%Y-%m-%d, %H heure'))
 print("### "*20)
 
-hours = datetime.now().hour 
+hours = datetime.now().hour
 affulence = math.cos(2*math.pi*((hours-20)/24)) + 1
 
 api_host = "http://localhost:8000"
@@ -22,49 +24,60 @@ payload_token = {
 response_token = requests.post(token_url, data=payload_token)
 
 
-
 def ask_for_recommandation(payload_recommendations, headers):
     recommendations_url = api_host + "/recommendations"
-    rating =  max(0, 
-                  min(5, 
-                      random.normalvariate(mu=2.5, sigma=2)
-                    )
-                )
+    rating = max(0,
+                 min(5,
+                     random.normalvariate(mu=2.5, sigma=2)
+                     )
+                 )
+    rating = round(rating, 2)
     payload_recommendations["list_movie"]["listMovie"][0]["rating"] = rating
-    response_recommendations = requests.post(recommendations_url, json=payload_recommendations, headers=headers)
+    response_recommendations = requests.post(
+        recommendations_url, json=payload_recommendations, headers=headers)
 
     if response_recommendations.status_code == 200:
         reco = response_recommendations.json().get("recommendation")
         print("Reçu avec comme film reco", reco)
         return reco["movieId"]
     else:
-        print(f"Erreur lors de l'envoi des recommandations: {response_recommendations.status_code}")
+        print(
+            f"Erreur lors de l'envoi des recommandations: {response_recommendations.status_code}")
 
 
-### ####
+#
 
+doc = Path("/home/romain/Documents/Formation/mai24_cmlops_film/data/processed")\
+    .glob("**/user_matrix.csv")
+doc = list(doc)
+users = pd.concat(
+    [pd.read_csv(d, sep=",") for d in doc],
+    axis=1
+)
+u_id = users["userId"]
 
 number_of_user = int(5 + 50*affulence + random.randint(a=1, b=25))
+random_users_list = u_id.sample(n=number_of_user)
+
 print("Affluence", number_of_user)
 
-for u in range(number_of_user):
+for user_example in random_users_list:
     time.sleep(max(0, random.normalvariate(mu=3, sigma=10)))
-    user_example = random.randint(a=1, b=130000)
     print("\n")
     print("### USER: ", user_example, " ###")
     payload_recommendations = {
-            "user": {
-                "userId": user_example
-            },
-            "list_movie": {
-                "listMovie": [
-                    {
-                        "moviesId": 0,
-                        "rating": 0
-                    }
-                ]
-            }
+        "user": {
+            "userId": user_example
+        },
+        "list_movie": {
+            "listMovie": [
+                {
+                    "moviesId": 0,
+                    "rating": 0
+                }
+            ]
         }
+    }
 
     number_rating = random.randint(a=2, b=10)
 
@@ -76,7 +89,11 @@ for u in range(number_of_user):
         }
         movieId = 1
         for i in range(number_rating):
-            payload_recommendations["list_movie"]["listMovie"][0]["moviesId"] = movieId
-            movieId = ask_for_recommandation(payload_recommendations=payload_recommendations, headers=headers)
+            if not movieId is None:
+                payload_recommendations["list_movie"]["listMovie"][0]["moviesId"] = movieId
+                movieId = ask_for_recommandation(
+                    payload_recommendations=payload_recommendations,
+                    headers=headers
+                )
     else:
         print(f"Erreur d'authentification: {response_token.status_code}")
